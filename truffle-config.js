a068968a61f8f6bb7ed07607d706bbae870aee93
mjerkov <47100526+mjerkov@users.noreply.github.com>
2020-06-19 19:06:42 +0200

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+   compilers: {
+    solc: {
+      version: "0.4.17" 
+    }
+   },	  
+   networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
